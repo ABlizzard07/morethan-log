ae644aacbd459ba55a5115e18c7643f10584d95a
ABlizzard07 <aashu.ananth@gmail.com>
2023-08-07 11:46:50 -0400

    site config test

--- site.config.js
+++ site.config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: [
     {
       name: `Learn More`,
-      href: "/",
+      href: "https://pnlowt0ixdq.typeform.com/to/njRuaarR",
     },
   ],
   // blog setting (required)
@@ -24,7 +24,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
