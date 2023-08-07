5741e21e84141d2c68440847d0488216432f48f1
ABlizzard07 <aashu.ananth@gmail.com>
2023-08-07 11:14:07 -0400

    config update

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Learn More`,
+      href: "/",
     },
   ],
   // blog setting (required)
